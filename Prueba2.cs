2d352cec5b3dcb7986f2e914112a74411f1bc434
LisandroFernandez2 <lisandrofernandezet32@gmail.com>
2026-03-19 11:07:22 -0300

    Add files via upload

---
+++ Prueba2.cs
@@ -0,0 +1,250 @@
+﻿using System.ComponentModel.Design;
+
+int x = 25;
+int y = 30;
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+if (x < y)
+{
+    Console.WriteLine(("Es falso"));
+
+}
+else {
+    Console.WriteLine("Es verdadero");
+}
+int z = 14;
+int a = 14;
+z = Convert.ToInt32(Console.ReadLine());
+a = Convert.ToInt32(Console.ReadLine());
+if (z <= a)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int b = 0;
+int c = 0;
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+if (b == c)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int d = 10;
+int e = 10;
+d = Convert.ToInt32(Console.ReadLine());
+e = Convert.ToInt32(Console.ReadLine());
+if (d != e)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int f = 8;
+int g = 3;
+f = Convert.ToInt32(Console.ReadLine());
+g = Convert.ToInt32(Console.ReadLine());
+if (f >= g)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int h = 10;
+int i = 10;
+h = Convert.ToInt32(Console.ReadLine());
+i = Convert.ToInt32(Console.ReadLine());
+if (h < i)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int j = 10;
+int k = 10;
+j = Convert.ToInt32(Console.ReadLine());
+k = Convert.ToInt32(Console.ReadLine());
+if (j != k)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int l = 10;
+int m = 10;
+l = Convert.ToInt32(Console.ReadLine());
+m = Convert.ToInt32(Console.ReadLine());
+if (l == m)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int n = 10;
+int ñ = 10;
+n = Convert.ToInt32(Console.ReadLine());
+ñ = Convert.ToInt32(Console.ReadLine());
+if (n > ñ)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+
+int o = 10;
+int p = 10;
+o = Convert.ToInt32(Console.ReadLine());
+p = Convert.ToInt32(Console.ReadLine());
+if (o < p)
+{
+    Console.WriteLine(("Es verdadero"));
+
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int q;
+int r;
+int s;
+int t;
+q = Convert.ToInt32(Console.ReadLine());
+r = Convert.ToInt32(Console.ReadLine());
+s = Convert.ToInt32(Console.ReadLine());
+t = Convert.ToInt32(Console.ReadLine());
+if (q > r && s < t)
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int u;
+int v;
+int w;
+int aa;
+u = Convert.ToInt32(Console.ReadLine());
+v = Convert.ToInt32(Console.ReadLine());
+w = Convert.ToInt32(Console.ReadLine());
+aa = Convert.ToInt32(Console.ReadLine());
+if (u == v || w != aa)
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int bb;
+int cc;
+bb = Convert.ToInt32(Console.ReadLine());
+cc= Convert.ToInt32(Console.ReadLine());
+if (!(bb == cc))
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int dd;
+int ee;
+int ff;
+int gg;
+dd = Convert.ToInt32(Console.ReadLine());
+ee = Convert.ToInt32(Console.ReadLine());
+ff = Convert.ToInt32(Console.ReadLine());
+gg = Convert.ToInt32(Console.ReadLine());
+if (dd > ee && !(ff > gg))
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int hh;
+int ii;
+int jj;
+int kk;
+hh = Convert.ToInt32(Console.ReadLine());
+ii = Convert.ToInt32(Console.ReadLine());
+jj = Convert.ToInt32(Console.ReadLine());
+kk = Convert.ToInt32(Console.ReadLine());
+if (hh + 2 > ii || jj * 2 < 8)
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int ll;
+bool mm;
+
+ll = Convert.ToInt32(Console.ReadLine());
+mm = Convert.ToBoolean(Console.ReadLine());
+
+if (ll > 30 && mm == true)
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int nn;
+
+nn = Convert.ToInt32(Console.ReadLine());
+
+if (!(nn >= 18))
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
+int ññ;
+int oo;
+int pp;
+int qq;
+ññ = Convert.ToInt32(Console.ReadLine());
+oo = Convert.ToInt32(Console.ReadLine());
+pp = Convert.ToInt32(Console.ReadLine());
+qq = Convert.ToInt32(Console.ReadLine());
+if (ññ / oo == 3 && pp % qq == 1)
+{
+    Console.WriteLine(("Es verdadero"));
+}
+else
+{
+    Console.WriteLine("Es falso");
+}
