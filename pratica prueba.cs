7e237e960833f3caa089ee86845757f9ec162035
LisandroFernandez2 <lisandrofernandezet32@gmail.com>
2026-04-05 18:47:50 -0300

    Add files via upload

---
+++ pratica prueba.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+void ejerciciop1 ()
+{
+    for (int x = 0; x <= 100; x += 3)
+    {
+        Console.WriteLine(x);
+    }
+} 
+ejerciciop1();
+
+void ejerciciop2()
+{
+    Console.WriteLine("Ingrese su edad");
+    int edad = Convert.ToInt32(Console.ReadLine());
+    if (edad > 18)
+    {
+
+        Console.WriteLine("Usted es mayor de edad");
+
+    }
+    else if (edad < 18)
+    {
+
+        Console.WriteLine("Usted es menor de edad");
+    }
+    else
+    {
+
+        Console.WriteLine("Usted tiene exactamente 18 años");
+
+
+    }
+    
+}
+ejerciciop2();
