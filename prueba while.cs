f76e1208402419532e3aa13850294e795dd60bcd
Lisandro <lisandrofernandezet32@gmail.com>
2026-04-30 09:29:25 -0300

    Add files via upload

---
+++ prueba while.cs
@@ -0,0 +1,45 @@
+﻿void ejercicio1()
+{
+    int numero = 2;
+    bool condicion = true;
+    while (condicion)
+    {
+        if (numero != 0 && numero != 1)
+        {
+            Console.WriteLine("Ingrese 0 o 1");
+            numero = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            condicion = false;
+        }
+    }
+
+
+
+}
+
+
+void ejercicio2()
+{
+    int numero = 3;
+    bool condicion = true;
+    while (condicion)
+    {
+       
+       
+            if (numero > 99 || numero < -99)
+            {
+                Console.WriteLine("Ingrese un numero de dos cifras");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                condicion = false;
+            }
+      
+    }
+
+
+}
+ejercicio2();
