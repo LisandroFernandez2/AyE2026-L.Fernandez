c069f503313ba2bdbfe278ae593a7b7285764b9e
Lisandro <lisandrofernandezet32@gmail.com>
2026-06-09 12:10:05 -0300

    Add files via upload

---
+++ SEGUNDOBLOQUE.cs
@@ -0,0 +1,17 @@
+﻿void segundobloqueA()
+{
+    Random aleatorio = new Random();
+    int numero = aleatorio.Next(0, 11);
+    int numeroingresado = 0;
+    while (numeroingresado != numero)
+    {
+        Console.WriteLine("Intente adivinar");
+        numeroingresado = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine("¡Felicidades, usted ha adivinado el numero!");
+
+
+
+
+}
+segundobloqueA();
