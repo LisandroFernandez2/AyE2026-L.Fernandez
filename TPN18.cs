8195386aae3d86d0aecc5818a19ba41a59fb6603
Lisandro <lisandrofernandezet32@gmail.com>
2026-06-30 13:25:20 -0300

    Add files via upload

---
+++ TPN18.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public struct Punto2D
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Punto2D(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public void Mostrar()
+            {
+                Console.WriteLine($"{X} {Y}");
+
+
+
+            }
+        }
+            static void Main(string[] args)
+            {
+                Punto2D punto = new Punto2D(5, 10);
+                punto.Mostrar();
+            }
+
+        
+    }
+}
