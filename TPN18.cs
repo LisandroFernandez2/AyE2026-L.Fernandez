25a665903875b7287a66daa79d52bf9a9cec3ad1
Lisandro <lisandrofernandezet32@gmail.com>
2026-06-30 18:27:36 -0300

    Update TPN18.cs

--- TPN18.cs
+++ TPN18.cs
@@ -23,10 +23,27 @@ namespace ConsoleApp2
 
             }
         }
+        public struct Dimensiones
+        {
+        public int Ancho { get; set; }
+        public int Alto { get; set; }
+
+
+        public Dimensiones(int ancho, int alto){
+        Ancho = ancho;
+        Alto = alto;
+          }
+        }
             static void Main(string[] args)
             {
                 Punto2D punto = new Punto2D(5, 10);
                 punto.Mostrar();
+                Dimensiones d1 = new Dimensiones(10, 20);
+                Dimensiones d2 = d1;
+                d2.Ancho = 99;
+                Console.WriteLine($"VALORES DE D1: {d1.Ancho},{d1.Alto}");
+                Console.WriteLine($"VALORES DE D2: {d2.Ancho},{d2.Alto}");
+                // Porque el sistema crea un nuevo espacio de memoria completamente nuevo para d2, sin modificar d1. Le pregunte a la IA xdxdxdxd
             }
 
         
