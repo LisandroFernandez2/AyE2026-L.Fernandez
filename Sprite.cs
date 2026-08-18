302e05601e6404bea85dc9b7e3dcb054ae2aba55
Lisandro <lisandrofernandezet32@gmail.com>
2026-08-18 13:25:56 -0300

    Add files via upload

---
+++ Sprite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+namespace Project1
+{
+    internal class Sprite
+    {
+        public Texture2D texture;
+        public Vector2 position;
+
+        public Sprite  (Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+    
+    }
+
+
+
+}
