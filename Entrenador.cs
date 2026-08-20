1f240a342226264aa329c069cac85fb787caa0f6
Lisandro <lisandrofernandezet32@gmail.com>
2026-08-20 12:05:25 -0300

    Add files via upload

---
+++ Entrenador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Entrenador
+    {
+        public string nombre { get; set; }
+        public int pokedolares { get; set; }
+        public Pokemon[] equipo { get; set; }
+
+        public Entrenador(string nombre, int pokedolares, Pokemon[] equipo)
+        {
+            this.nombre = nombre;
+            this.pokedolares = pokedolares;
+            this.equipo = equipo;
+        }
+
+
+
+    }
+}
