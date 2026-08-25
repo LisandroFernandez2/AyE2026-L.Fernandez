1e32cf8f5cd889e8704d05eabb27bbaccba61e52
Lisandro <lisandrofernandezet32@gmail.com>
2026-08-25 13:27:07 -0300

    Add files via upload

---
+++ Perro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Perro
+    {
+        int edad { get; set; }
+        string dueño { get; set; }
+        string raza { get; set; }
+
+        public Perro(int edad,string dueño, string raza)
+        {
+            this.edad = edad;
+            this.dueño = dueño;
+            this.raza = raza;
+        }
+ 
+    } 
+}
