170a51af6d4057d5e1e9bffd90f746c96843bb1e
Lisandro <lisandrofernandezet32@gmail.com>
2026-09-01 11:47:55 -0300

    Add files via upload

---
+++ Simulacro prueba.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Ubicacion> HistorialUbicaciones = new Stack<Ubicacion>();
+            Ubicacion PrimeraUbicacion = new Ubicacion(5, 5, "Bosque");
+            Ubicacion SegundaUbicacion = new Ubicacion(2, 5, "Selva");
+            Ubicacion TerceraUbicacion = new Ubicacion(15, 25, "Cueva");
+            HistorialUbicaciones.Push(PrimeraUbicacion);
+            HistorialUbicaciones.Push(SegundaUbicacion);
+            HistorialUbicaciones.Push(TerceraUbicacion);
+            foreach (Ubicacion si in HistorialUbicaciones)
+            {
+                Console.WriteLine("Zona: " + si.NombreZona);
+            }
+
+
+            for (int i = 0; i < HistorialUbicaciones.Count; i++)
+            {
+                Console.Write("Usted retrocedio de " + HistorialUbicaciones.Peek().NombreZona);
+                HistorialUbicaciones.Pop();
+                Console.Write(" A " + HistorialUbicaciones.Peek().NombreZona);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Ubicacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Ubicacion
+    {
+  public int x { get; set; }
+        public int y { get; set; }
+     public   string NombreZona { get; set; }
+        public Ubicacion(int x, int y, string NombreZona)
+        {
+            this.x = x;
+            this.y = y;
+            this.NombreZona = NombreZona;
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
